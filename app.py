2bd73a17ebe12938c4f46664734c9cfcb6f6b61e
8BitJonny <timon.christiansen@code.berlin>
2018-05-02 17:21:23 +0200

    Added template structure, navbar, routes, bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")

651352c2300e0afa4164447838764ccf51dc6c9e
Timon Christiansen <jonny@JonnysMcBookPro.fritz.box>
2018-05-06 15:15:13 +0200

    FIrst Database Setup, Displaying it on the index page

---
+++ DBsetup.py
@@ -0,0 +1,24 @@
+import sqlite3
+conn = sqlite3.connect("company.db")
+
+c = conn.cursor()
+
+#Create Table
+c.execute('''DROP TABLE employees''')
+
+c.execute('''CREATE TABLE employees
+             (id integer primary key autoincrement, firstname text, lastname text, birthdate text, age integer, sex text, workload_per_week real, work_group text)''')
+
+# Insert a row of data
+c.execute("INSERT INTO employees VALUES (0,'Hans','Petersen','1987-01-05','31','M',40,'IT')")
+c.execute("INSERT INTO employees VALUES (1,'Peter','Hansen','1966-06-11','52','M',20,'Sales')")
+c.execute("INSERT INTO employees VALUES (2,'Julia','Johannsen','1982-11-03','36','W',40,'Marketing')")
+c.execute("INSERT INTO employees VALUES (3,'Hannah','Thomsen','1996-04-09','21','W',20,'Sales')")
+c.execute("INSERT INTO employees VALUES (4,'Christoph','Tran','1993-02-02','25','M',15,'IT')")
+
+# Save (commit) the changes
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, flash, redirect, render_template, request, session
+import sqlite3
 
 app = Flask(__name__)
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    with sqlite3.connect("company.db") as con:
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM employees")
+        db_employees = cursor.fetchall()
+    return render_template("index.html", db_employees=db_employees)
 
 @app.route("/add")
 def add():
